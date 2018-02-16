e48f040167a35719b2d98e30296e43f52c20a48e
Samuel Chia <cheeeya@berkeley.edu>
2018-02-15 22:18:30 -0800

    fix port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 // const http = require('http').Server(app)
 const path = require('path');
 const fetch = require('node-fetch');
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const _ = require('lodash');
 
 app.use(express.static('public'));
